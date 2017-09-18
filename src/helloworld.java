e06b3c712ecee2480aa526e07fdf87e50df59411
christianfal <s429532@ocsb.local>
2017-09-18 13:09:05 -0400

    Start

---
+++ src/helloworld.java
@@ -0,0 +1,22 @@
+/**
+ * This is my hello world program
+ * @author s429532
+ *
+ */
+public class helloworld {
+
+	public static void main(String[] args) {
+		//This is a single comment
+		/*
+		 * this is 
+		 * a multiline
+		 * comment
+		 */
+		System.out.println("Hello, I'm jyan");
+		System.out.println("this is my first program");
+		System.out.println("isn't this fun ??");
+		System.out.println("he said \"do this now!");
+		System.out.println("its u boi");
+	}
+
+}
